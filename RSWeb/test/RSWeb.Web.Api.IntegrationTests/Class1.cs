a37a0877b2cee7d248e4c59a187b1742c96d453e
Lena-PC\IvanyCH008 <chernets.ivan@gmail.com>
2017-07-10 23:17:07 +0300

    Начало

---
+++ RSWeb/test/RSWeb.Web.Api.IntegrationTests/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RSWeb.Web.Api.IntegrationTests
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
